f25f8fb3593b458fc9fca24b83c4f69575ee7fea
BriceTchams <michaeltchamou393@gmail.com>
2026-02-12 11:01:11 +0100

    article

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/login' ,
+  function (){
+    return response()->json(
+        [
+            'nom' => 'bricoe'
+        ]
+    );
+  }
+
+);
